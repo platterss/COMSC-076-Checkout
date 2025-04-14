3d7fff1f35c720bf098128e138889ff0619bf1f9
platterss <kenny.do7306@gmail.com>
2025-04-13 20:04:44 -0700

    Add customer functionality

--- src/Checkout.java
+++ src/Checkout.java
@@ -1,5 +1,11 @@
 public class Checkout {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final int SIMULATION_TIME = 7200; // 2 hours expressed in seconds
+        final int NUM_CUSTOMERS = 200; // may be unused in final program, just used for testing
+
+        for (int i = 0; i < NUM_CUSTOMERS; i++) {
+            Customer customer = new Customer();
+            System.out.println(customer);
+        }
     }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,52 @@
-public class Customer {
+import java.util.Random;
 
+public class Customer {
+    // Constant static variables
+    private static final int NUM_ITEMS_LOWER_BOUND = 1;
+    private static final int NUM_ITEMS_UPPER_BOUND = 25;
+
+    private static final int TIME_PER_ITEM_LOWER_BOUND = 1;
+    private static final int TIME_PER_ITEM_UPPER_BOUND = 6;
+
+    private static final int TIME_TO_PAY_LOWER_BOUND = 2;
+    private static final int TIME_TO_PAY_UPPER_BOUND = 45;
+
+    // Member variables
+    private final int numItems;
+    private final int timePerItem;
+    private final int timeToPay;
+
+    // Constructors
+    public Customer() {
+        Random random = new Random();
+
+        numItems = random.nextInt(NUM_ITEMS_UPPER_BOUND - NUM_ITEMS_LOWER_BOUND + 1) + NUM_ITEMS_LOWER_BOUND;
+        timePerItem = random.nextInt(TIME_PER_ITEM_UPPER_BOUND - TIME_PER_ITEM_LOWER_BOUND + 1) + TIME_PER_ITEM_LOWER_BOUND;
+        timeToPay = random.nextInt(TIME_TO_PAY_UPPER_BOUND - TIME_PER_ITEM_LOWER_BOUND + 1) + TIME_TO_PAY_LOWER_BOUND;
+    }
+
+    public Customer(int numItems, int timePerItem, int timeToPay) {
+        this.numItems = numItems;
+        this.timePerItem = timePerItem;
+        this.timeToPay = timeToPay;
+    }
+
+    // Accessors
+    public int getNumItems() {
+        return numItems;
+    }
+
+    public int getTimePerItem() {
+        return timePerItem;
+    }
+
+    public int getTimeToPay() {
+        return timeToPay;
+    }
+
+    // Debug
+    @Override
+    public String toString() {
+        return "Customer: " + numItems + " item(s), " + timePerItem + " seconds/item, " + timeToPay + " seconds to pay";
+    }
 }
